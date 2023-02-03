8269675a28a4bcbd9f84949716ba09411cd6fe1f
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2023-02-03 18:33:56 +0100

    pawn movement function in private

--- includes/pieces/pawn.hpp
+++ includes/pieces/pawn.hpp
@@ -6,14 +6,14 @@ class Pawn : public Piece
 {
   private:
     int value;
-
-  public:
-    Pawn(int x, int y, int color);
     void checkEnPassant(array2d<Tile, 8, 8> board, PieceMove &result, int x, int y);
     void checkPromotion(array2d<Tile, 8, 8> board, PieceMove &result, int x, int y);
     void checkMove(array2d<Tile, 8, 8> board, PieceMove &result, int x, int y);
     void checkDoubleMove(array2d<Tile, 8, 8> board, PieceMove &result, int x, int y);
     void checkCapture(array2d<Tile, 8, 8> board, PieceMove &result, int x, int y);
+
+  public:
+    Pawn(int x, int y, int color);
     ~Pawn();
     virtual PieceMove isValidMove(array2d<Tile, 8, 8> board, int x, int y) override;
 };
